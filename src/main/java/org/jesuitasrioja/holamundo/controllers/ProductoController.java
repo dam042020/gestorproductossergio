a412c26778b5a61fadc1ecf82b06c332e29297fe
dam042020 <dam04.2020.jesuitas@gmail.com>
2020-12-17 08:11:38 +0100

    New Commit

--- src/main/java/org/jesuitasrioja/holamundo/controllers/ProductoController.java
+++ src/main/java/org/jesuitasrioja/holamundo/controllers/ProductoController.java
@@ -4,17 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.jesuitasrioja.holamundo.modelo.Producto;
+import org.jesuitasrioja.holamundo.repository.IProductosRepository;
+import org.jesuitasrioja.holamundo.repository.ProductosRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST })
 public class ProductoController {
+
+	private IProductosRepository pr = new ProductosRepository();
 
 	// Toda la lista productos BBDD
 	@GetMapping("/productos")
@@ -31,20 +38,18 @@ public class ProductoController {
 
 	@GetMapping("/producto/{id}")
 	public Producto getProducto(@PathVariable String id) {
-		// TODO
-		return new Producto("1", "tomate", 1.30);
+		return pr.getById(id);
 	}
 
 	@GetMapping("/producto")
 	public Producto getProducto2(@RequestParam String id) {
-		// TODO
-		return new Producto("1", "tomate", 1.30);
+		return pr.getById(id);
 	}
 
 	@PostMapping("/producto")
 	public String postProducto(@RequestBody Producto nuevoProducto) {
 		// TODO
-		return "Aniadido: " + nuevoProducto.hashCode();
+		return String.valueOf(pr.aniadirProducto(nuevoProducto));
 	}
 
 	@PutMapping("/producto")
@@ -53,10 +58,10 @@ public class ProductoController {
 		return null;
 	}
 
-	@DeleteMapping("/producto")
-	public Producto deleteProducto() { // TODO aniadir
+	@DeleteMapping("/producto/{id}")
+	public String deleteProducto(@PathVariable String id) { // TODO aniadir
 		// TODO
-		return null;
+		return String.valueOf(pr.eliminarProducto(id));
 	}
 
 //	@GetMapping("/saludos/{nombre}")
